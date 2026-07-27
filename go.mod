a63140db36dad7419fb98740586c5775d74db7d2
gihon <gihon1018@qq.com>
2026-07-28 07:52:26 +0800

    开发集群相关功能，暂存进度

--- go.mod
+++ go.mod
@@ -33,5 +33,6 @@ require (
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
+	gopkg.in/ini.v1 v1.67.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/util/ini_util.go
@@ -0,0 +1,75 @@
+package util
+
+import (
+	"fmt"
+
+	"gopkg.in/ini.v1"
+)
+
+type IniFile struct {
+	file *ini.File
+	path string
+}
+
+func NewIniFile(path string) (*IniFile, error) {
+	file, err := ini.Load(path)
+	if err != nil {
+		return nil, fmt.Errorf("加载 ini 文件 %s 失败: %w\n", path, err)
+	}
+	return &IniFile{
+		file: file,
+		path: path,
+	}, nil
+}
+
+func (i *IniFile) Path() string {
+	return i.path
+}
+
+func (i *IniFile) HasSection(section string) bool {
+	return i.file.HasSection(section)
+}
+
+func (i *IniFile) HasKey(section, key string) bool {
+	s, err := i.file.GetSection(section)
+	if err != nil {
+		return false
+	}
+	return s.HasKey(key)
+}
+
+func (i *IniFile) GetString(section, key, def string) string {
+	return i.file.Section(section).Key(key).MustString(def)
+}
+
+func (i *IniFile) GetInt(section, key string, def int) int {
+	return i.file.Section(section).Key(key).MustInt(def)
+}
+
+func (i *IniFile) GetBool(section, key string, def bool) bool {
+	return i.file.Section(section).Key(key).MustBool(def)
+}
+
+func (i *IniFile) Set(section, key, value string) {
+	i.file.Section(section).Key(key).SetValue(value)
+}
+
+func (i *IniFile) DeleteKey(section, key string) error {
+	s, err := i.file.GetSection(section)
+	if err != nil {
+		return fmt.Errorf("获取 section 区块 %s 失败: %w\n", section, err)
+	}
+	s.DeleteKey(key)
+	return nil
+}
+
+func (i *IniFile) Save() error {
+	if i.path == "" {
+		return fmt.Errorf("ini 文件路径为空")
+	}
+	return i.file.SaveTo(i.path)
+}
+
+func (i *IniFile) SaveTo(savePath string) error {
+	return i.file.SaveTo(savePath)
+}
